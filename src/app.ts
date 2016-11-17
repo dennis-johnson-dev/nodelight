bb13240566e5a516059553c4dd9e39c29088d620
John T Skarbek <jtslear@gmail.com>
2016-11-16 21:22:46 -0500

    makes a web listener

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,12 @@
-function hello(compiler: string) {
-  console.log(`Hello from ${compiler}`);
+import * as http from 'http';
+
+function hello(request: http.IncomingMessage, response: http.ServerResponse) {
+  response.end('Hello World');
 }
-hello("TypeScript");
+
+const server = http.createServer(hello);
+
+const port = 3000
+server.listen(port, function() {
+  console.log("%s TypeScript listening on %s", process.pid, port);
+});
